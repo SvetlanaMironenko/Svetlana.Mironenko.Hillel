425c25240ead4377fee8de0ef0236950f06e3998
Svetlana Mironenko <svetic.syd@gmail.com>
2023-05-10 15:20:34 +0200

    Added my homework lesson10

--- src/hillel/lesson06/MyGame.java
+++ src/hillel/lesson06/MyGame.java
@@ -5,18 +5,13 @@ public class MyGame {
     public static void main(String[] args) {
         PokerCards pCards = new PokerCards();
 
-        Player playerOne = new Player("Nikolas");
-        Player playerTwo = new Player("Pedro");
-        Player playerThree = new Player("Daniil");
-        Player playerFour = new Player("Andrian");
-
         PokerGame game = new PokerGame();
         game.registerCards(pCards);
 
-        game.registerPlayer(playerOne);
-        game.registerPlayer(playerTwo);
-        game.registerPlayer(playerThree);
-        game.registerPlayer(playerFour);
+        game.registerPlayer(new Player("Nikolas"))
+                .registerPlayer(new Player("Pedro"))
+                .registerPlayer(new Player("Daniil"))
+                .registerPlayer(new Player("Andrian"));
 
         System.out.println("1. Prepare for game");
         game.prepareForGame();
